3dc69152a7d865f5a8cc1ba474b3480c9e10ba5d
Pandeysat <sk3036321@gmail.com>
2022-09-05 17:32:45 +0530

    first commit

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cars/details":["static\u002Fchunks\u002Fpages\u002Fcars\u002Fdetails.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcars\u002Fdetails"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
